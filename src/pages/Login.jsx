99d79c4996ebadb8893c26487f6b79f90749d2f3
nguyensongnghiem <nguyensongnghiem@gmail.com>
2024-11-19 13:36:26 +0700

    useLocation to redirect to last page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { useAuth } from "../contexts/authContext";
 import { postData } from "../services/apiService";
-import { NavLink, useNavigate } from "react-router-dom";
+import { NavLink, useNavigate, useLocation } from "react-router-dom";
 import axios from "../libs/axios/axiosConfig";
 import { Button, Typography } from "@material-tailwind/react";
 import { jwtDecode } from "jwt-decode";
@@ -9,29 +9,24 @@ import useRefreshToken from "../hooks/useRefreshToken";
 const Login = () => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
-  const [success, setSuccess] = useState(false);
   const [error, setError] = useState("");
   const { auth, setToken } = useAuth();
   const navigate = useNavigate();
+  const location = useLocation();
   const { refreshToken } = useRefreshToken();
-
+  const from = location.state?.from?.pathname || "/";
   const handleSubmit = async (e) => {
     e.preventDefault();
     const submitCredentials = { username: username, password: password };
     try {
-      setSuccess(false);
+
       const response = await axios.post("auth/login", submitCredentials, {
         withCredentials: true,
       });
       console.log(response.data);
       const accessToken = response.data.accessToken;
-      const claims = jwtDecode(accessToken);
-      console.log(claims);
-      console.log(new Date(claims.exp * 1000));
-
       setToken(accessToken);
-      console.log(auth);
-      setSuccess(true);
+      navigate(from, { replace: true });
     } catch (error) {
       console.log(error.response);
 
@@ -49,53 +44,36 @@ const Login = () => {
   return (
     <div className="flex min-h-screen items-center justify-center bg-gray-100">
       <div className="w-full max-w-md space-y-3 rounded-lg bg-white p-8 shadow-md">
-        {success ? (
+        <h2 className="text-center text-2xl font-bold">Đăng Nhập</h2>
+        {error && <div className="text-red-500">{error}</div>}
+        <form onSubmit={handleSubmit} className="space-y-5">
           <div>
-            <Typography variant="h5" className="mb-4">
-              Bạn đã đăng nhập thành công!
-            </Typography>
-            <NavLink to={"/"}>
-              <a className="text-blue-500 underline hover:text-blue-700">
-                Về trang chủ
-              </a>
-            </NavLink>
+            <label className="block text-sm font-medium">Tên đăng nhập</label>
+            <input
+              type="text"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+              required
+              className="w-full rounded-md border border-gray-300 p-2"
+            />
           </div>
-        ) : (
-          <>
-            <h2 className="text-center text-2xl font-bold">Đăng Nhập</h2>
-            {error && <div className="text-red-500">{error}</div>}
-            <form onSubmit={handleSubmit} className="space-y-5">
-              <div>
-                <label className="block text-sm font-medium">
-                  Tên đăng nhập
-                </label>
-                <input
-                  type="text"
-                  value={username}
-                  onChange={(e) => setUsername(e.target.value)}
-                  required
-                  className="w-full rounded-md border border-gray-300 p-2"
-                />
-              </div>
-              <div>
-                <label className="block text-sm font-medium">Mật khẩu</label>
-                <input
-                  type="password"
-                  value={password}
-                  onChange={(e) => setPassword(e.target.value)}
-                  required
-                  className="w-full rounded-md border border-gray-300 p-2"
-                />
-              </div>
-              <button
-                type="submit"
-                className="w-full rounded-md bg-blue-600 py-2 text-white hover:bg-blue-700"
-              >
-                Đăng Nhập
-              </button>
-            </form>
-          </>
-        )}
+          <div>
+            <label className="block text-sm font-medium">Mật khẩu</label>
+            <input
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+              className="w-full rounded-md border border-gray-300 p-2"
+            />
+          </div>
+          <button
+            type="submit"
+            className="w-full rounded-md bg-blue-600 py-2 text-white hover:bg-blue-700"
+          >
+            Đăng Nhập
+          </button>
+        </form>
       </div>
     </div>
   );
--- src/routes/PrivateRoutes.jsx
+++ src/routes/PrivateRoutes.jsx
@@ -1,40 +1,13 @@
 import { useEffect, useState } from 'react'
-import { Navigate, Outlet } from 'react-router-dom'
+import { Navigate, Outlet, useLocation } from 'react-router-dom'
 import { useAuth } from '../contexts/authContext'
 import axios from 'axios'
 export default function PrivateRoutes() {
     const { auth } = useAuth();
-    const [isAuthenticated, setIsAuthenticated] = useState(false);
-    const [isLoading, setIsLoading] = useState(true); // Thêm trạng thái loading
+    const location = useLocation();
     const token = auth?.accessToken
-    // useEffect(() => {
-    //     const checkAuthenticated = async () => {
-    //         setIsLoading(true); // Bắt đầu quá trình xác thực
-    //         try {
-    //             const response = await axios.get('http://localhost:8080/api/auth/validate-token', {
-    //                 headers: {
-    //                     Authorization: `Bearer ${token}` // Gửi token nếu cần
-    //                 }
-    //             });
-    //             console.log('Authentication check successful');
-    //             console.log(response.data);
-    //             setIsAuthenticated(true);
-    //         } catch (error) {
-    //             console.error('Authentication check failed', error);
-    //             setIsAuthenticated(false);
-    //         } finally {
-    //             setIsLoading(false); // Kết thúc quá trình xác thực
-    //         }
-    //     };
-
-    //     checkAuthenticated();
-    // }, [token]);
-
-    // if (isLoading) {
-    //     return <div>Loading...</div>; // Có thể hiển thị một spinner hoặc thông báo loading
-    // }
-
+   
     return (
-        token ? <Outlet /> : <Navigate to="/login" />
+        token ? <Outlet /> : <Navigate to="/login" state= {{ from: location }} replace />
     );
 }
